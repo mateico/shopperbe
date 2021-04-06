df6c08007c1c74474a263b268ff42bcdf6e0eb94
Mateo Rial <mateo.rial@gmail.com>
2021-04-05 23:12:39 -0300

    filters, sortings and limits added

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,33 +3,91 @@ const Product = require('./../models/productModel');
 // param middleware has the val parameter
 // the return will make the next() method not reachable.
 
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    /* result: products.length,
-    data: {
-      products,
-    }, */
-  });
+exports.getAllProducts = async (req, res) => {
+  try {
+    //  BUILD QUERY
+    //1) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    //1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    let query = Product.find(JSON.parse(queryStr));
+
+    //2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //3) FIELD LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split('.').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //  EXECUTE QUERY
+    const products = await query;
+
+    res.status(200).json({
+      status: 'success',
+      result: products.length,
+      data: {
+        products,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getProduct = (req, res) => {
-  console.log(req.params);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      id: req.params,
-    },
-  });
+exports.getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.updateProduct = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'Updated tour here...',
-    },
-  });
+exports.updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createProduct = async (req, res) => {
@@ -43,13 +101,21 @@ exports.createProduct = async (req, res) => {
       },
     });
   } catch (err) {
-    res.status(400).json({ status: 'fail', message: 'Invalid data sent!' });
+    res.status(400).json({ status: 'fail', message: err });
   }
 };
 
-exports.deleteProduct = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
